12a1fc49acd5137e5e461e252503b6808f1dffc2
abbasmortazavi <abbassmortazavi@gmail.com>
2026-07-17 12:11:02 +0330

    init projects files

---
+++ cmd/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+
+	fmt.Println("Hello World")
+	var app application
+	//set config
+
+	//read the command line
+
+	//connect to the database
+	app.Domain = "example.com"
+	log.Println("Listening on port " + strconv.Itoa(port))
+	//start the webserver
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
